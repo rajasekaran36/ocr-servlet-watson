12bd56e9142a9046d17155fc82c7343e8e843fa8
rajasekaran <proffraja@gmail.com>
2020-10-15 01:49:42 +0530

    'improve'

--- src/main/java/com/raja/ibm/ocrapp/ImageToTextConverter.java
+++ src/main/java/com/raja/ibm/ocrapp/ImageToTextConverter.java
@@ -16,6 +16,11 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,17 +28,16 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ImageToTextConverter extends HttpServlet {
     @Override
-
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException,ParseException {
         System.out.println("Hello World!");
         // File f = new File("src/main/java/com/raja/wt/u4/sax/ap.PNG");
         ServletFileUpload sf = new ServletFileUpload(new DiskFileItemFactory());
-        List<FileItem> multifiles=null;
+        List<FileItem> multifiles = null;
         try {
             multifiles = sf.parseRequest(req);
         } catch (FileUploadException e1) {
@@ -47,21 +51,26 @@ public class ImageToTextConverter extends HttpServlet {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            
+
         }
-        File f = new File("src/resources/"+fileName);
+        File f = new File("src/resources/" + fileName);
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://max-ocr.codait-prod-41208c73af8fca213512856c7a09db52-0000.us-east.containers.appdomain.cloud/model/predict");
+        HttpPost httpPost = new HttpPost(
+                "http://max-ocr.codait-prod-41208c73af8fca213512856c7a09db52-0000.us-east.containers.appdomain.cloud/model/predict");
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-        //builder.addBinaryBody("image",new FileInputStream(f), ContentType.APPLICATION_OCTET_STREAM, f.getName());
-        builder.addBinaryBody("image",new FileInputStream(f), ContentType.APPLICATION_OCTET_STREAM, f.getName());
-        
+        // builder.addBinaryBody("image",new FileInputStream(f),
+        // ContentType.APPLICATION_OCTET_STREAM, f.getName());
+        builder.addBinaryBody("image", new FileInputStream(f), ContentType.APPLICATION_OCTET_STREAM, f.getName());
+
         httpPost.addHeader("accept", "application/json");
         HttpEntity multipart = builder.build();
         httpPost.setEntity(multipart);
         CloseableHttpResponse response = httpClient.execute(httpPost);
         HttpEntity entity = response.getEntity();
-        resp.getWriter().println(EntityUtils.toString(entity));
+        String jsonData = EntityUtils.toString(entity);
+        
+        resp.getWriter().println(jsonData);
         f.delete();
+
     }
 }
